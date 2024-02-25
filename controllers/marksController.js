cf3a9dc44c8a768f24f39df022312ac266f12e3c
Obada Al-Maleh <70761777+Obaa10@users.noreply.github.com>
2024-02-25 14:40:34 +0300

    Update marksController.js

--- controllers/marksController.js
+++ controllers/marksController.js
@@ -29,33 +29,3 @@ export async function professorAddMarks(req, res) {
     return false;
   }
 }
-
-// const originalData = "Original data to sign";
-// const { signature, hash } = createSignature(originalData);
-// console.log("Generated Signature:", forge.util.bytesToHex(signature));
-// console.log("Generated hash:", forge.util.bytesToHex(hash));
-// console.log("Generated hash:", hash);
-// const modifiedData = "Modified data";
-// const isVerified = verifySignature(modifiedData, signature, hash);
-// console.log("Is Modified Data Verified with Original Signature?", isVerified);
-// Function to create an RSA signature using node-forge
-// export function createSignature(data) {
-//   try {
-//     const keypair = forge.pki.rsa.generateKeyPair({ bits: 2048 });
-//     var privateKey = forge.pki.privateKeyToPem(keypair.privateKey);
-//     privateKey = forge.pki.privateKeyFromPem(privateKey);
-
-//     // Create SHA256 hash of the data
-//     const md = forge.md.sha256.create();
-//     md.update(data, "utf8");
-//     const hash = md.digest().getBytes();
-
-//     // Sign the hash using the private key
-//     const signature = privateKey.sign(md);
-
-//     return { signature, hash };
-//   } catch (error) {
-//     console.error("Signature creation error:", error);
-//     return null;
-//   }
-// }
